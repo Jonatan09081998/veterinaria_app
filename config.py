bce1445549a055c8d47017a566973c1ad5915983
Jonatan09081998 <barbosajonatan85@gmail.com>
2025-08-12 16:59:36 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'flaskdb.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_urlsafe(24)
-
+# prueba 2
 #Comandos para descargar en instalar todas las librerias offline  
 #python -m pip download -r requirements.txt -d librerias
 #pip install --no-index --find-links=librerias -r requirements.txt
